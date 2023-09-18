2fa8e40db26633e20000223d3ef5594fedd0a85e
Fil <f.visocchi@stonehex.com>
2023-09-18 17:53:36 +0200

    added very first note for U5

---
+++ include_paths.h
@@ -0,0 +1,34 @@
+/* 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Project : A_os_U575_devel 
+*/
+/*
+ * include_paths.h
+ *
+ *  Created on: Sep 18, 2023
+ *      Author: fil
+ */
+
+#ifndef INCLUDE_PATHS_H_
+#define INCLUDE_PATHS_H_
+/*
+ *
+ * STM32U5 includes
+../../A_Libraries/ExternalLibraries/STM32U5/STM32_USB_Device_Library/Class/CDC
+../../A_Libraries/Drivers/STM32U5/Usb_Device/Target
+../../A_Libraries/ExternalLibraries/STM32U5/STM32_USB_Device_Library/Core
+
+
+*/
+#endif /* INCLUDE_PATHS_H_ */
